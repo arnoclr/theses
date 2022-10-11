49d8fba6af5b3447c117702d166e35ae8b9fc7de
Arno Cellarier <bonjour@arnocellarier.fr>
2022-10-11 09:25:53 +0200

    fix: htmlspecialchars in theses title tooltip

--- src/Views/results.php
+++ src/Views/results.php
@@ -24,7 +24,7 @@
                 <ul>
                     <?php foreach ($moreAccurate as $these) : ?>
                         <li>
-                            <a title="<?= $these->title ?>" class="ellipsis" href="/?action=view&tid=<?= $these->iddoc ?>">
+                            <a title="<?= htmlspecialchars($these->title) ?>" class="ellipsis" href="/?action=view&tid=<?= $these->iddoc ?>">
                                 <?= $these->title ?>
                             </a>
                         </li>
