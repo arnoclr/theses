22768102292b657aefde98a10cdee0604ee30252
Arno Cellarier <bonjour@arnocellarier.fr>
2022-10-15 18:31:45 +0200

    feat: add cloudflare web analytics

--- src/Views/footer.php
+++ src/Views/footer.php
@@ -2,6 +2,9 @@
 <script src="https://cdn.jsdelivr.net/npm/material-dynamic-colors@0.0.10/dist/cdn/material-dynamic-colors.min.js" integrity="sha256-z92LW2bxx5MARSvtWPwx5lbA23+ckWW6MX2EBYt0qlU=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/highcharts@10.2.1/highcharts.js" integrity="sha256-pMM+8rU0FYzOb1cBRvygzjF/A0xFNc40SSh1yNYkTbs=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/highcharts@10.2.1/modules/map.js" integrity="sha256-mtScLQfuCFAJx+PowDDuMp0CXwmoE7d3IvdZX3fHeZI=" crossorigin="anonymous"></script>
+<!-- Cloudflare Web Analytics -->
+<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "47bf47ac7b664839bafa450605bf1741"}'></script>
+<!-- End Cloudflare Web Analytics -->
 
 <script>
     document.addEventListener('DOMContentLoaded', async () => {
