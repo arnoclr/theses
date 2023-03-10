8e0710c0b1e2209dc86115c1a09f9e483b5e125a
Arno Cellarier <bonjour@arnocellarier.fr>
2023-03-10 17:26:29 +0100

    fix: remove duplicate alerts

--- index.php
+++ index.php
@@ -121,16 +121,31 @@ switch ($action) {
         if ($sessionData === false) {
             $hint = "Essayez d'ouvrir le lien dans le même navigateur que celui utilisé pour créer l'alerte. Si cela ne fonctionne pas, alors le mail de confirmation a expiré.";
             require "src/Views/alert.php";
+            require "src/Views/footer.php";
             exit;
         }
 
         if ($sessionData['_token'] !== $token) {
             $hint = "Le lien de confirmation est invalide.";
             require "src/Views/alert.php";
+            require "src/Views/footer.php";
             exit;
         }
 
         $q = $sessionData['q'];
+
+        $alertAlreadyExists = $pdo->prepare('SELECT * FROM alerts WHERE email = :email AND q = :q')
+            ->execute([
+                'email' => $email,
+                'q' => $q,
+            ]);
+
+        if ($alertAlreadyExists) {
+            $hint = "Vous avez déjà une alerte pour cette recherche.";
+            require "src/Views/alert.php";
+            require "src/Views/footer.php";
+            exit;
+        }
 
         $pdo->prepare('INSERT INTO alerts (email, q, unsubscribe_token, created_at) VALUES (:email, :q, :token, NOW())')
             ->execute([
