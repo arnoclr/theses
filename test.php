f06cf3cf66e7d511cd59253bdf88446e089cc6e7
Arno Cellarier <bonjour@arnocellarier.fr>
2023-04-02 11:45:10 +0200

    fix: delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// autoload php classes
-foreach (glob("app/class/*.php") as $class) {
-    require_once $class;
-}
-
-$searcher = new Searcher(null);
-echo $searcher->before(2019)->after(2018)->fromAuthor('John')->limit(5)->sortedAlphabetically()->_debug();
