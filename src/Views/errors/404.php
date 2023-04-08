456bedd0a429a34a2c3cf74fcb9811aa88a631ce
Léon <93096590+Leon-ED@users.noreply.github.com>
2023-04-08 11:45:48 +0200

    Fix : faute d'orthographe
    Le verbe "réessayer" est construit à partir du préfixe "ré-" qui signifie "de nouveau" et du verbe "essayer". En français, lorsque l'on ajoute un préfixe au début d'un verbe, le préfixe ne prend pas de trait d'union et le verbe conserve sa forme de base.
    
    Par conséquent, "réessayer" est la forme correcte du verbe, quelle que soit la personne ou le temps utilisé. Il n'y a donc pas de forme "rééssayer".

--- src/Views/errors/404.php
+++ src/Views/errors/404.php
@@ -1,3 +1,3 @@
 <h1>Page non trouvée</h1>
 <small>Erreur 404</small>
-<p>La page demandée n'existe pas ou plus. Vérifiez l'orthographe et rééssayez.</p>
+<p>La page demandée n'existe pas ou plus. Vérifiez l'orthographe et réessayez.</p>
